35322c90b040a5342bcb79888791d0c26d951174
karuppasamythiyagu <ajsamy2156@gmail.com>
2022-03-01 17:42:04 +0530

    project added

---
+++ src/org/sample/Sample.java
@@ -0,0 +1,15 @@
+package org.sample;
+
+public class Sample {
+
+	private void sampleOne() {
+		System.out.println("Sample One");
+
+	}
+
+	private void sampleTwo() {
+		System.out.println("Sample Two");
+
+	}
+
+}
